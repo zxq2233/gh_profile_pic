36d9c510491d8296e694b61239c0891a7fd49c58
balajiceg <balaji.9th@gmail.com>
2017-02-26 12:18:21 +0530

    as

--- web/index.php
+++ web/index.php
@@ -2,6 +2,8 @@
 <?php
 
 require('../vendor/autoload.php');
+ini_set('post_max_size', '20M');
+ini_set('upload_max_filesize', '20M');
 if(isset($_POST["submit"])){
 try 
 { 
@@ -13,32 +15,37 @@ $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-  //  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
        // echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
     } else {
-       // echo "File is not an image.";
-        //$uploadOk = 0;
+        echo "File is not an image.";
+        $uploadOk = 0;
     }
 }
 	
+	
+	/*
 // Check if file already exists
-//if (file_exists($target_file)) {
-//    echo "Sorry, file already exists.".$imageFileType ;
-//    $uploadOk = 0;
-//}
+if (file_exists($target_file)) {
+   echo "Sorry, file already exists.".$imageFileType ;
+   $uploadOk = 0;
+}
 // Check file size
-//if ($_FILES["fileToUpload"]["size"] > 500000) {
-//    echo "Sorry, your file is too large.";
-//    $uploadOk = 0;
-//}
-// Allow certain file formats
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+ Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
     echo "Sorry, only JPG, JPEG,PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
+	*/
+	
+	
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
